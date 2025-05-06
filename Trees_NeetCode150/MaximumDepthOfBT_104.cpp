f2bf8c1976b983c3808bb7ef03bbb2d8d69fa1f2
dinumerceanu <dinu.eu@yahoo.com>
2025-05-06 21:34:09 +0300

    MaximumDepthOfBT(104)

---
+++ Trees_NeetCode150/MaximumDepthOfBT_104.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode* root) {
+    if (root) {
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    } else return 0;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(17);
+
+    cout<<maxDepth(root);
+
+    return 0;
+}
