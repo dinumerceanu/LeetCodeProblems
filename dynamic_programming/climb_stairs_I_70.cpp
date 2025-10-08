c237c45ea51deb972d560a44747693dff11cee79
dinumerceanu <dinu.eu@yahoo.com>
2025-10-08 09:27:08 +0300

    climb stairs I 70

---
+++ dynamic_programming/climb_stairs_I_70.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int climbStairs(int n) {
+    if (n < 3) {
+        return n;
+    }
+
+    vector<int> dp(n + 1);
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
+int main() {
+    cout << climbStairs(5) << endl;
+
+    return 0;
+}
