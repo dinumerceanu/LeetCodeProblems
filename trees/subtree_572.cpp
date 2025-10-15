0afb31e19298078d963f1f87fb31468bfd40025b
dinumerceanu <dinu.eu@yahoo.com>
2025-10-15 12:17:05 +0300

    subtree 572

---
+++ trees/subtree_572.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q) {
+        return true;
+    }
+    
+    if (!p || !q || p->val != q->val) {
+        return false;
+    }
+
+    return isSubtree(p->left, q->left) && isSubtree(p->right, q->right);
+}
+
+bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    if (!subRoot) {
+        return true;
+    }
+
+    if (!root) {
+        return false;
+    }
+
+    if (isSameTree(root, subRoot)) {
+        return true;
+    }
+
+    return isSameTree(root->left, subRoot) || isSameTree(root->right, subRoot);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(5);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(2);
+
+    TreeNode* subRoot = new TreeNode(4);
+    subRoot->left = new TreeNode(1);
+    subRoot->right = new TreeNode(2);
+
+    cout << isSubtree(root, subRoot) << endl;
+
+    return 0;
+}
