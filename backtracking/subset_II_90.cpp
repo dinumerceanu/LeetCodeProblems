3b291e12c69606f5b74697cc6bdd375fc87a0f9c
dinumerceanu <dinu.eu@yahoo.com>
2025-10-15 15:35:31 +0300

    subset II 90

---
+++ backtracking/subset_II_90.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void backtrack(int start_index, vector<int>& nums, vector<int>& current_subset, vector<vector<int>>& results) {
+    results.push_back(current_subset); 
+
+    for (int i = start_index; i < nums.size(); i++) {
+        if (i > start_index && nums[i] == nums[i - 1]) {
+            continue;
+        }
+
+        current_subset.push_back(nums[i]);
+
+        backtrack(i + 1, nums, current_subset, results);
+        
+        current_subset.pop_back();
+    }
+}
+
+int main() {
+    vector<int> nums = {1, 2, 2};
+    vector<vector<int>> results;
+    vector<int> current_subset;
+    int start_index = 0;
+    sort(nums.begin(), nums.end());
+
+    backtrack(start_index, nums, current_subset, results);
+
+    for (int i = 0; i < results.size(); i++) {
+        for (int j = 0; j < results[i].size(); j++) {
+            cout << results[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
