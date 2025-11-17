0f6fa29f6d84fb0cfec3af887db1819a151a67bb
dinumerceanu <dinu.eu@yahoo.com>
2025-11-17 17:36:04 +0200

    reverse integer 7

---
+++ math/reverse_integer_7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int reverse(int x) {
+    int res = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+
+        if (res > INT_MAX / 10 || res < INT_MIN / 10) {
+            return 0;
+        }
+
+        if (res == INT_MAX / 10 && digit > 7) {
+            return 0;
+        }
+
+        if (res == INT_MIN / 10 && digit < -8)  {
+            return 0;
+        }
+
+        res = res * 10 + digit;
+        x /= 10;
+    }
+
+    return res;
+}
+
+int main() {
+    cout << reverse(123) << endl;
+
+    return 0;
+}
